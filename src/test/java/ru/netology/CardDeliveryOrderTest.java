ba1772a2b37bf1e896799dd580a475929605daa9
OlgaKP <89009086+OlgaKP@users.noreply.github.com>
2021-12-19 21:46:30 +0400

    Add tests and .yml

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -42,84 +42,136 @@ public class CardDeliveryOrderTest {
         $("[data-test-id='notification'] .notification__content")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate));
     }
-}
-//    @Test
-//    void shouldSendWithoutCity() {
-//        //Configuration.holdBrowserOpen = true;
-//        open("http://localhost:9999/");
-//        $("[data-test-id='date'] input")
-//                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), "25.12.2021");
-//        $("[data-test-id='name'] input").setValue("Ия Ли");
-//        $("[data-test-id='phone'] input").setValue("+78001112233");
-//        $(".checkbox").click();
-//        $(".button").click();
-//        $("[data-test-id='city'].input_invalid .input__sub")
-//                .shouldHave(Condition.text("Поле обязательно для заполнения"));
-//    }
-//
-//    @Test
-//    void shouldSendWithoutDate() {
-//        //Configuration.holdBrowserOpen = true;
-//        open("http://localhost:9999/");
-//        $("[data-test-id='city'] input").setValue("Ижевск");
-//        $("[data-test-id='name'] input").setValue("Ия Ли");
-//        $("[data-test-id='phone'] input").setValue("+78001112233");
-//        $(".checkbox").click();
-//        $(".button").click();
-//        $("[data-test-id='notification'] .notification__title").shouldHave(Condition.text("Успешно!"),
-//                Duration.ofSeconds(15));
-//        $("[data-test-id='notification'] .notification__content")
-//                .shouldHave(Condition.text("Встреча успешно забронирована на 22.12.2021")); //дата актуальна только в текущий день!!
-//    }
-//
-//    @Test
-//    void shouldSendWithoutName() {
-//        //Configuration.holdBrowserOpen = true;
-//        open("http://localhost:9999/");
-//        $("[data-test-id='city'] input").setValue("Ижевск");
-//        $("[data-test-id='date'] input")
-//                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), "25.12.2021");
-//        $("[data-test-id='phone'] input").setValue("+78001112233");
-//        $(".checkbox").click();
-//        $(".button").click();
-//        $("[data-test-id='name'].input_invalid .input__sub")
-//                .shouldHave(Condition.text("Поле обязательно для заполнения"));
-//    }
-//
-//    @Test
-//    void shouldSendWithoutPhone() {
-//        //Configuration.holdBrowserOpen = true;
-//        open("http://localhost:9999/");
-//        $("[data-test-id='city'] input").setValue("Ижевск");
-//        $("[data-test-id='date'] input")
-//                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), "25.12.2021");
-//        $("[data-test-id='name'] input").setValue("Ия Ли");
-//        $(".checkbox").click();
-//        $(".button").click();
-//        $("[data-test-id='phone'].input_invalid .input__sub")
-//                .shouldHave(Condition.text("Поле обязательно для заполнения"));
-//    }
-//
-//    @Test
-//    void shouldSendWithoutCheckbox() {
-//        //Configuration.holdBrowserOpen = true;
-//        open("http://localhost:9999/");
-//        $("[data-test-id='city'] input").setValue("Ижевск");
-//        $("[data-test-id='date'] input")
-//                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), "25.12.2021");
-//        $("[data-test-id='name'] input").setValue("Ия Ли");
-//        $("[data-test-id='phone'] input").setValue("+78001112233");
-//        $(".button").click();
-//        $("[data-test-id='agreement'] .checkbox__text")
-//                .shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
-//    }
-//
-//    @Test
-//    void shouldSendNothing() {
-//        //Configuration.holdBrowserOpen = true;
-//        open("http://localhost:9999/");
-//        $(".button").click();
-//        $("[data-test-id='city'].input_invalid .input__sub")
-//                .shouldHave(Condition.text("Поле обязательно для заполнения"));
-//    }
 
+    @Test
+    void shouldSendWithoutCity() {
+        open("http://localhost:9999/");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), planningDate);
+        $("[data-test-id='name'] input").setValue("Ия Ли");
+        $("[data-test-id='phone'] input").setValue("+78001112233");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='city'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSendWithoutBelorussianCity() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Минск");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), planningDate);
+        $("[data-test-id='name'] input").setValue("Ия Ли");
+        $("[data-test-id='phone'] input").setValue("+78001112233");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='city'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldSendWithCityOnEnglish() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Moscow");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), planningDate);
+        $("[data-test-id='name'] input").setValue("Ия Ли");
+        $("[data-test-id='phone'] input").setValue("+78001112233");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='city'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldSendWithoutDate() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Ижевск");
+        $("[data-test-id='name'] input").setValue("Ия Ли");
+        $("[data-test-id='phone'] input").setValue("+78001112233");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='notification'] .notification__title").shouldHave(Condition.text("Успешно!"),
+                Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(3))); //дата актуальна только в текущий день!!
+    }
+
+    @Test
+    void shouldSendWithMeetingDateIn1Day() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Ижевск");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), generateDate(1));
+        $("[data-test-id='name'] input").setValue("Ия Ли");
+        $("[data-test-id='phone'] input").setValue("+78001112233");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='date'] .input_invalid .input__sub")
+                .shouldHave(Condition.text("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldSendWithMeetingDateIn3Day() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Ижевск");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), generateDate(3));
+        $("[data-test-id='name'] input").setValue("Ия Ли");
+        $("[data-test-id='phone'] input").setValue("+78001112233");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='notification'] .notification__title").shouldHave(Condition.text("Успешно!"),
+                Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(3)));
+    }
+
+    @Test
+    void shouldSendWithoutName() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Ижевск");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), planningDate);
+        $("[data-test-id='phone'] input").setValue("+78001112233");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='name'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSendWithoutPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Ижевск");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), planningDate);
+        $("[data-test-id='name'] input").setValue("Ия Ли");
+        $(".checkbox").click();
+        $(".button").click();
+        $("[data-test-id='phone'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldSendWithoutCheckbox() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Ижевск");
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE), planningDate);
+        $("[data-test-id='name'] input").setValue("Ия Ли");
+        $("[data-test-id='phone'] input").setValue("+78001112233");
+        $(".button").click();
+        $("[data-test-id='agreement'] .checkbox__text")
+                .shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+
+    @Test
+    void shouldSendNothing() {
+        open("http://localhost:9999/");
+        $(".button").click();
+        $("[data-test-id='city'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+}
